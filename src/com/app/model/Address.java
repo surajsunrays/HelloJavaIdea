f8f3f2dec1cd79a1cb44244006f9490df171ac48
suraj <suraj.sunrays86@outlook.com>
2018-08-28 21:22:58 +0530

    Address and Employee Classes are created.

---
+++ src/com/app/model/Address.java
@@ -0,0 +1,8 @@
+package com.app.model;
+
+public class Address {
+    private int aid;
+    private String loc;
+    private int pinCode;
+}
+
---
+++ src/com/app/model/Employee.java
@@ -0,0 +1,6 @@
+package com.app.model;
+public class Employee {
+    private int empId;
+    private String empName;
+    private double empSal;
+}
