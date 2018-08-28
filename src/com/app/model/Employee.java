9333a78649ead47ee5892190234495e846b0c903
suraj <suraj.sunrays86@outlook.com>
2018-08-28 21:34:52 +0530

    Employee class getter , setter and Multiplicity Applied.

--- src/com/app/model/Employee.java
+++ src/com/app/model/Employee.java
@@ -1,6 +1,66 @@
 package com.app.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "emptab")
 public class Employee {
+    @Id
+    @Column(name = "eid")
     private int empId;
+    @Column(name = "ename")
     private String empName;
+    @Column(name = "esal")
     private double empSal;
+
+    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "eidfk")
+    private List<Address> addr = new ArrayList<>(0);
+
+    public Employee() {
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getEmpSal() {
+        return empSal;
+    }
+
+    public void setEmpSal(double empSal) {
+        this.empSal = empSal;
+    }
+
+    public List<Address> getAddr() {
+        return addr;
+    }
+
+    public void setAddr(List<Address> addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empSal=" + empSal +
+                ", addr=" + addr +
+                '}';
+    }
 }
